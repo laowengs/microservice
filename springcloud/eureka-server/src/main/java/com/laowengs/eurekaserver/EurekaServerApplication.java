d649dfca16658e05f39e73cd09d493e1e7a7aa34
laowengs <369196439@qq.com>
2021-05-26 17:50:07 +0800

    修改注册中心地址

--- springcloud/eureka-server/src/main/java/com/laowengs/eurekaserver/EurekaServerApplication.java
+++ springcloud/eureka-server/src/main/java/com/laowengs/eurekaserver/EurekaServerApplication.java
@@ -1,11 +1,14 @@
 package com.laowengs.eurekaserver;
 
+import com.laowengs.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Import;
 
 @EnableEurekaServer
 @SpringBootApplication
+@Import(WebSecurityConfig.class)
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
