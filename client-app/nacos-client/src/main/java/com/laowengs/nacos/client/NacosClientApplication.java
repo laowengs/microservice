d4d7ab56320d3b9a46b30ba7f2f12c11b7584c15
laowengs <369196439@qq.com>
2022-03-18 16:29:08 +0800

    删除无用的import

--- client-app/nacos-client/src/main/java/com/laowengs/nacos/client/NacosClientApplication.java
+++ client-app/nacos-client/src/main/java/com/laowengs/nacos/client/NacosClientApplication.java
@@ -1,20 +1,13 @@
 package com.laowengs.nacos.client;
 
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.logging.LoggerConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Comparator;
 
 @SpringBootApplication
 @EnableDiscoveryClient
